f1a7921ff11f2a43a9e9a8c42452eaf859c0c668
James Munnelly <james.munnelly@gmail.com>
2015-05-04 16:41:28 +0100

    use settings.AUTH_USER_MODEL instead of User model

--- voting/models.py
+++ voting/models.py
@@ -7,8 +7,8 @@ from datetime import datetime
 from django.utils.encoding import python_2_unicode_compatible
 from django.contrib.contenttypes import generic
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.auth.models import User
 from django.db import models
+from django.conf import settings
 
 try:
     from django.utils.timezone import now
@@ -28,7 +28,7 @@ class Vote(models.Model):
     """
     A vote on an object by a User.
     """
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
     object = generic.GenericForeignKey('content_type', 'object_id')
